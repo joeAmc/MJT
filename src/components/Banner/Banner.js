df336e8abb65a85c62648bbdeb2d92e12e23b364
Joe McIlroy <94605341+joeAmc@users.noreply.github.com>
2024-04-11 22:16:12 +0100

    banner style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,8 +7,8 @@ const Banner = () => {
   console.log("Banner.js: Banner component rendered!");
   return (
     <section>
-      <div className="banner-container h-screen w-screen">
-        <div className="banner-content">
+      <div className="banner-container h-screen w-screen flex justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+        <div className="banner-content flex  flex-col justify-center items-center">
           <Image className="banner-logo h-[10em] w-[10em]" src={Logo} alt="" />
           <h1 className="text-2xl">Midas Touch Jewellery</h1>
         </div>
