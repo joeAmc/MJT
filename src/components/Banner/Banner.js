e9c7ebba7c756fbea9ad7ca520e8d1b1700c7e8e
Joe McIlroy <94605341+joeAmc@users.noreply.github.com>
2024-04-12 08:29:41 +0100

    banner img

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./Banner.css";
 import Logo from "../../assets/Logo.png";
+import bannerImg from "../../assets/banner.jpg";
 import Image from "next/image";
 
 const Banner = () => {
-  console.log("Banner.js: Banner component rendered!");
   return (
     <section>
-      <div className="banner-container h-screen w-screen flex justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <div className="banner-container h-screen w-screen flex justify-center bg-banner">
+        <Image
+          className="object-cover absolute -z-50"
+          src={bannerImg}
+          alt="banner image"
+        />
         <div className="banner-content flex  flex-col justify-center items-center">
-          <Image className="banner-logo h-[10em] w-[10em]" src={Logo} alt="" />
+          <Image
+            className="banner-logo h-[10em] w-[10em]"
+            src={Logo}
+            alt="logo"
+          />
           <h1 className="text-2xl">Midas Touch Jewellery</h1>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    fontSize: {
-      "2xl": [
-        "32px",
-        {
-          lineHeight: "2rem",
-          letterSpacing: "-0.01em",
-          fontWeight: "200",
-        },
-      ],
-      "3xl": [
-        "3rem",
-        {
-          lineHeight: "2.25rem",
-          letterSpacing: "-0.02em",
-          fontWeight: "700",
-        },
-      ],
-    },
+    extend: {},
   },
   plugins: [],
 };
