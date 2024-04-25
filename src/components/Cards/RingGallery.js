b426171c6ca432a655ad94c193aa154259e5c6b1
Joe McIlroy <94605341+joeAmc@users.noreply.github.com>
2024-04-25 22:07:04 +0100

    update gallery

--- src/components/Cards/RingGallery.js
+++ src/components/Cards/RingGallery.js
@@ -38,28 +38,34 @@ const ringImages = [
 
 const RingGallery = ({ title }) => {
   const [isGallery, setIsGallery] = useState(true);
+  const [closeUpImage, setcloseUpImage] = useState();
 
-  const toggleZoom = () => {
-    setIsGallery(!isGallery);
+  const toggleZoom = (id) => {
+    ringImages.map((ring) => {
+      if (ring.id === id) {
+        setcloseUpImage(ring.src);
+      }
+    }),
+      setIsGallery(!isGallery);
   };
 
+  console.log(closeUpImage, "closeUpImage");
   const closeZoom = () => {
     setIsGallery(!isGallery);
   };
 
   return (
-    <div className="mx-32 my-6 relative">
-      <h1 className="text-4xl mt-6">Ring name</h1>
-      <div className="gallery-container grid gap-4 grid-cols-6  auto-rows-[300px] auto-cols-[300px]">
+    <div className="mx-32 my-10 relative">
+      <div className="gallery-container grid gap-4 grid-cols-6  auto-rows-[300px] auto-cols-[300px] w-full">
         {ringImages.map(({ id, src, title, colspan, rowspan }) => {
           return (
             <div
-              className={`gallery-item-${id} ${colspan} ${rowspan}`}
+              className={`gallery-item-${id} ${colspan} ${rowspan} overflow-hidden`}
               key={id}
             >
               <Image
-                className="h-full rounded-lg object-cover	cursor-pointer"
-                onClick={toggleZoom}
+                className="h-full rounded-lg object-cover	cursor-pointer ease-in duration-300 hover:scale-105"
+                onClick={() => toggleZoom(id)}
                 src={src}
                 alt={title}
               />
@@ -69,17 +75,21 @@ const RingGallery = ({ title }) => {
       </div>
       {!isGallery && (
         <div onClick={closeZoom}>
-          <div className="close-up rounded-lg bg-white w-3/5 h-3/5 mt-[160px] z-50 fixed top-0 bottom-0 left-0 right-0 mx-auto">
+          <div className=" close-up rounded-lg bg-white w-3/5 h-3/5 mt-[160px] z-50 fixed top-0 bottom-0 left-0 right-0 mx-auto">
             <Image
-              className="h-full rounded-lg object-cover"
-              src={Rings}
+              className="h-full rounded-lg object-cover "
+              src={closeUpImage}
               alt={title}
             />
           </div>
           <div className="background h-full w-full fixed top-0 left-0 bg-black-rgba "></div>
         </div>
       )}
-      <h1>buy here</h1>
+      <div className="ring-text  ">
+        <h1 className="text-4xl mt-3 mb-3 ">Swirl</h1>
+        <p className="">Very nice organic inspired silver ring</p>
+      </div>
+      <button className="btn mt-4 ">Shop</button>
     </div>
   );
 };
